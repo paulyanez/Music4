31a98784b3a7ea2ae76436c5f0a11f63f0845e65
paulyanez <34757740+paulyanez@users.noreply.github.com>
2023-06-26 15:16:17 -0400

    Add files via upload

---
+++ Music4/Music4/Music4App.swift
@@ -0,0 +1,18 @@
+//
+//  Music4App.swift
+//  Music4
+//
+//  Created by Paul Yanez on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Music4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
